cf106b4c68174d583355ce8b5d95ebcb20516afe
이지현 <dlwlgus4553@gmail.com>
2019-05-16 21:34:19 +0900

    go to others

--- HelloExpress/routes/shelf.js
+++ HelloExpress/routes/shelf.js
@@ -15,40 +15,50 @@ var connection = mysql.createConnection({
 
 var obj = {};
 
-var userID = -1
+var userID =-1
+var ownerID=2;
+var mine =0;
 
 
 /* GET shelf page. */
 router.get('/', function(req, res, next) {
+  mine =0 ;
   if(!req.session.userID) res.redirect('user/login');
   else{
-    console.log("session" + req.session.userID);
+    var ownerID =  req.param("sid");
+    var userID = req.session.userID;
+    if(ownerID == userID || ownerID == -1){
+    // 남서재, 내서재 구분
+      mine=1;
+      ownerID=userID;
+    }  
+    //console.log("session" + req.session.ownerID);
     console.log("shelf");
-    userID = req.session.userID;
+    //ownerID = req.session.ownerID;
 
     sql = 
     //[0]user info
-      "SELECT * FROM User WHERE " + "'" + userID + "' = user_index;" + 
+      "SELECT * FROM User WHERE " + "'" + ownerID + "' = user_index;" + 
       //[1] follower count
-     " SELECT COUNT (user_index) as count FROM Subscribe WHERE " + "'" + userID + "' = user_index;"+ 
+     " SELECT COUNT (user_index) as count FROM Subscribe WHERE " + "'" + ownerID + "' = user_index;"+ 
      //[2]following count
-     " SELECT COUNT (follower_id) as count FROM Subscribe WHERE " + "'" + userID + "' = follower_id;"+
-      "SELECT * FROM Post WHERE" + "'" + userID + "' = user_index;"+
-      "SELECT  Post.post_id, Post.title, User.nickName, User.user_index FROM Subscribe INNER JOIN User ON Subscribe.user_index = User.user_index INNER JOIN Post ON Subscribe.user_index = Post.user_index WHERE Subscribe.follower_id = "+"'" +  userID+"';"+
+     " SELECT COUNT (follower_id) as count FROM Subscribe WHERE " + "'" + ownerID + "' = follower_id;"+
+      "SELECT * FROM Post WHERE" + "'" + ownerID + "' = user_index;"+
+      "SELECT  Post.post_id, Post.title, User.nickName, User.user_index FROM Subscribe INNER JOIN User ON Subscribe.user_index = User.user_index INNER JOIN Post ON Subscribe.user_index = Post.user_index WHERE Subscribe.follower_id = "+"'" +  ownerID+"';"+
       //좋아요 수
-      "SELECT COUNT (love_id) as count FROM Love WHERE type='book' AND '" + userID + "' = user_index AND  love_status = 1;"+
+      "SELECT COUNT (love_id) as count FROM Love WHERE type='book' AND '" + ownerID + "' = user_index AND  love_status = 1;"+
       //[6]읽은 책 수
-      "SELECT COUNT (book_read_id) as count FROM Book_Read WHERE return_date is not NULL AND '" + userID + "' = user_index;"+
+      "SELECT COUNT (book_read_id) as count FROM Book_Read WHERE return_date is not NULL AND '" + ownerID + "' = user_index;"+
       //[7]인용문 수
-      "SELECT COUNT (quotation_id) as count FROM Quotation WHERE '" + userID + "' = user_index;"+
+      "SELECT COUNT (quotation_id) as count FROM Quotation WHERE '" + ownerID + "' = user_index;"+
       //[8]내가 좋아한 책
-      //"SELECT * FROM Book INNER JOIN Love ON Love.book_id = Book.book_id WHERE '" + userID + "' = user_index AND  '" + 1 + "' = love_status;"
+      //"SELECT * FROM Book INNER JOIN Love ON Love.book_id = Book.book_id WHERE '" + ownerID + "' = user_index AND  '" + 1 + "' = love_status;"
       //[8]대여중인 도서
-      "SELECT * FROM Book_Read INNER JOIN Book ON Book_Read.book_id = Book.book_id  WHERE return_date is NULL AND '" + userID + "' = user_index;"+
+      "SELECT * FROM Book_Read INNER JOIN Book ON Book_Read.book_id = Book.book_id  WHERE return_date is NULL AND '" + ownerID + "' = user_index;"+
       //[9]카테고리 수
-      "SELECT DISTINCT bookshelf_title FROM Bookshelf WHERE '" + userID + "' = user_index;"+
+      "SELECT DISTINCT bookshelf_title FROM Bookshelf WHERE '" + ownerID + "' = user_index;"+
       //[10]카테고리 별 도서
-      "SELECT * FROM Bookshelf INNER JOIN Book ON Bookshelf.book_id = Book.book_id WHERE '" + userID + "' = user_index;";
+      "SELECT * FROM Bookshelf INNER JOIN Book ON Bookshelf.book_id = Book.book_id WHERE '" + ownerID + "' = user_index;";
 
    
 
@@ -67,9 +77,10 @@ router.get('/', function(req, res, next) {
           quotation: result[7],
           borrowings : result[8],
           category : result[9],
-          categorized : result[10]
-
+          categorized : result[10],
+          mine : {value : mine}
         };
+        console.log(obj);
         res.render('shelf',obj);
       }
     })
@@ -82,8 +93,7 @@ router.get('/', function(req, res, next) {
 
 /* GET shelf/post page. */
 router.get('/post', function(req, res, next) {
-
-    userID = req.session.userID;
+    //ownerID = req.session.ownerID;
     var pid =  req.param("pid");
     var uid =  req.param("uid");
     sql = 
@@ -105,7 +115,7 @@ router.get('/post', function(req, res, next) {
 /* GET shelf/post/delete page. */
 router.get('/post/delete', function(req, res, next) {
 
-  userID = req.session.userID;
+  // = req.session.ownerID;
   var pid =  req.param("pid");
   var uid =  req.param("uid");
   sql = "DELETE FROM Post WHERE '" + pid + "' = post_id;"
@@ -120,7 +130,7 @@ router.get('/post/delete', function(req, res, next) {
 
 /* GET shelf/post/delete page. */
 router.get('/post/setpost', function(req, res, next) {
-  userID = req.session.userID;
+  //ownerID = req.session.ownerID;
   var pid =  req.param("pid");
 
   if(pid==-1){
@@ -147,12 +157,12 @@ router.get('/post/setpost', function(req, res, next) {
 router.post('/post/setpost', function(req, res, next) {
   var body = req.body;
 
-  userID = req.session.userID;
+  //ownerID = req.session.ownerID;
   var pid =  req.param("pid");
   res.render('shelf/setpost');
   if(pid==-1){
     connection.query("INSERT INTO Post (user_index, title,contents) VALUES (?,?,?)", [
-      userID, body.title, body.contents
+      ownerID, body.title, body.contents
     ]);
   }else{
     sql = "UPDATE Post SET title = '" + body.title+"', contents = '"+ body.contents +"' WHERE '" + pid + "' = post_id;"
